08665e25fee86a78b3a6d9a702f6bbcd00f849b6
Alex Junior <alexjuniorkuss@outlook.com>
2021-08-11 10:14:07 -0300

    em desenvolvimento .data

--- Dupraa/Data/Context/BaseContext.cs
+++ Dupraa/Data/Context/BaseContext.cs
@@ -1,12 +1,20 @@
-﻿using System;
+﻿using Data.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Data.Context
 {
-    class BaseContext
+    public class BaseContext : DbContext
     {
+        public DbSet<Lanches> Lanches { get; set; }
+        public DbSet<Bebidas> Bebidas { get; set; }
+
+        public BaseContext():base("RefeicoesConnections")
+        {
+        }
     }
 }
---
+++ Dupraa/Data/Models/Lanches.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Models
+{
+    public class Lanches : BaseModel
+    {
+        public string Tipo { get; set; }
+        public int QtdItens { get; set; }
+        public decimal Valor { get; set; }
+    }
+}
